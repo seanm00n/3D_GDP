c7bcf85981545b7e8f6af7c6433ef842271da350
SeanM <deccj97@naver.com>
2022-09-15 11:19:19 +0900

    0915 class

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
