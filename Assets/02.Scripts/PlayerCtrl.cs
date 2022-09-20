07d615fa5625dfd4c27a44b83077839703ac7f40
seanm00n <deccj97@naver.com>
2022-09-20 11:55:43 +0900

    0920 class
    feat
    playerctrl.cs : player movement

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    private void Start()
-    {
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private Transform tr;
+    public float moveSpeed = 1.0f;
 
-    }
-    private void FixedUpdate()
+    void Start()
     {
-        
+        tr = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("h" + h.ToString());
+        Debug.Log("v" + h.ToString());
+        Vector3 moveDir = (Vector3.forward*v) + (Vector3.right*h);
+        //tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
+        //tr.Translate(Vector3.right * moveSpeed * h * Time.deltaTime, Space.Self);
+        tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
     }
 }
