a8b8f80be01ff805e8db8725df41979ad0bc1e01
seanm00n <deccj97@naver.com>
2022-09-29 11:07:20 +0900

    0929 class
    feat
    - Edit Take001 (separated)
    - animation : four direction

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[System.Serializable]
+public class Anim { 
+    public AnimationClip Idle;
+    public AnimationClip runForward;
+    public AnimationClip runBackward;
+    public AnimationClip runRight;
+    public AnimationClip runLeft;
+}
+
 public class PlayerCtrl : MonoBehaviour
 {
     private float h;
@@ -11,6 +20,10 @@ public class PlayerCtrl : MonoBehaviour
     private float y;
     private float moveSpeed;
     private float rotSpeed;
+
+    private Animation _animation;
+    public Anim anim;
+    
 
     private Transform tr;
 
@@ -29,6 +42,9 @@ public class PlayerCtrl : MonoBehaviour
     {
         Init();
         tr = GetComponent<Transform>();
+        _animation  = GetComponentInChildren<Animation>();
+        _animation.clip = anim.Idle;
+        _animation.Play();
     }
     void Update()
     {
@@ -53,5 +69,25 @@ public class PlayerCtrl : MonoBehaviour
         tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
 
         tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+        //
+        if(v >= 0.1f)
+        {
+            _animation.CrossFade(anim.runForward.name, 0.3f);
+        } else if(v <= -0.1f)
+        {
+            _animation.CrossFade(anim.runBackward.name, 0.3f);
+        }
+        else if(h >= 0.1f)
+        {
+            _animation.CrossFade(anim.runRight.name, 0.3f);
+        }
+        else if(h <= -0.1f)
+        {
+            _animation.CrossFade(anim.runLeft.name, 0.3f);
+        }
+        else
+        {
+            _animation.CrossFade(anim.Idle.name, 0.3f);
+        }
     }
 }
