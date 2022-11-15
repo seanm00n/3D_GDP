df24663e02bd1471be7eb54c743af7b67ab437db
seanm00n <deccj97@naver.com>
2022-11-15 11:54:07 +0900

    1115 class
    feat
    - UI
    - Button
    - UIManager

---
+++ Assets/02.Scripts/UIMgr.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMgr : MonoBehaviour
+{
+    public void OnClickStartBtn()
+    {
+        SceneManager.LoadScene("scPlay");
+        Debug.Log("Click Button");
+    }
+}
+
